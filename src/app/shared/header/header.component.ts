4032f5e1ae97233af76343d946f0a9a69f7c467b
Joby Mathews <jobyywilson@gmail.com>
2026-09-25 23:50:45 +0530

    deaths

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,16 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  private routerSubscription?: Subscription;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     window.addEventListener('scroll', this.scroll, true); 
+    this.routerSubscription = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.closeNavBar();
+      }
+    });
   }
 
   ngOnDestroy() {
       window.removeEventListener('scroll', this.scroll, true);
-
+      this.routerSubscription?.unsubscribe();
   }
 
   scroll = (event: any): void => {
@@ -50,6 +58,17 @@ export class HeaderComponent implements OnInit {
     let navBarMobile = document.querySelector<HTMLElement>( '.mobile-nav-toggle i' );
     navBarMobile?.classList.toggle('bi-list')
     navBarMobile?.classList.toggle('bi-x')
+  }
+
+  closeNavBar(): void {
+    const navbarElement = document.getElementsByClassName('navbar')[0] as HTMLElement | undefined;
+    navbarElement?.classList.remove('navbar-mobile');
+
+    const navBarMobile = document.querySelector<HTMLElement>('.mobile-nav-toggle i');
+    navBarMobile?.classList.remove('bi-x');
+    navBarMobile?.classList.add('bi-list');
+
+    document.querySelector('.ul-dropdown')?.classList.remove('dropdown-active');
   }
 
 }
